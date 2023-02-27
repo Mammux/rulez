2dac9a8fac4600477d39d9445d878dacf8d422ff
Magnus Alvestad <magnus.alvestad@avinor.no>
2023-02-27 15:43:52 +0100

    Unworking changes

--- lib.rs
+++ lib.rs
@@ -13,16 +13,26 @@ struct Field<'a> {
     entry: &'a Entry
 }
 
+struct RuleMaps<'a> {
+    pub entries: FrozenMap<String, Box<Entry>>,
+    pub fields: FrozenMap<String, Box<Field<'a>>>,
+}
+
+impl RuleMaps<'_> {
+    fn new() -> RuleMaps<'static> {
+	RuleMaps { entries: FrozenMap::new(), fields: FrozenMap::new() }
+    }
+}
+
 fn main() {
-    let entry_map: FrozenMap<String, Box<Entry>> = FrozenMap::new();
-    entry_map.insert("test".to_string(), Box::new(Entry("e1".to_string())));
+    let mut maps = RuleMaps::new();
+    
+    maps.entries.insert("test".to_string(), Box::new(Entry("e1".to_string())));
+    maps.fields.insert("test".to_string(), Box::new(Field{id: "f1".to_string(), entry: maps.entries.get("test").unwrap()}));
 
-    let field_map: FrozenMap<String, Box<Field>> = FrozenMap::new();
-    field_map.insert("test".to_string(), Box::new(Field{id: "f1".to_string(), entry: entry_map.get("test").unwrap()}));
+    let _t = maps.entries.get("test");
 
-    let _t = entry_map.get("test");
-
-    println!("Entry: {:?}", entry_map["test"]);
-    println!("Field: {:?}", field_map["test"]);
+    println!("Entry: {:?}", maps.entries["test"]);
+    println!("Field: {:?}", maps.fields["test"]);
 }
 
